44817ce20be6f5ac82860ed72df174dfd8744e51
Mukund K <koladiyamukund58@gmail.com>
2024-03-12 17:19:04 +0530

    Slider

--- lib/attendance.dart
+++ lib/attendance.dart
@@ -168,73 +168,262 @@ late String imjUrl;
 List<Map<String, dynamic>> studentList = [];
 List<int> clickCounts = [];
 
+// class Attendance extends StatefulWidget {
+//   final program;
+//
+//   const Attendance({super.key, this.program});
+//
+//   void main() async {
+//     WidgetsFlutterBinding.ensureInitialized();
+//     Firebase.initializeApp(
+//       options: DefaultFirebaseOptions.currentPlatform,
+//     );
+//     //  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,overlays: SystemUiOverlay.values);
+//   }
+//
+//   @override
+//   _AttendanceState createState() => _AttendanceState();
+// }
+//
+// class _AttendanceState extends State<Attendance> {
+//   bool passwordObscured = true;
+//   List<int> clickCounts = [];
+//
+//   void _changeColorAndText(int index) {
+//     setState(() {
+//       clickCounts[index] = (clickCounts[index] + 1) % 3;
+//     });
+//   }
+//
+//   Color _getButtonColor(int index) {
+//     switch (clickCounts[index]) {
+//       case 1:
+//         return Colors.green;
+//       case 2:
+//         return Colors.red;
+//       default:
+//         return Colors.white;
+//     }
+//   }
+//
+//   String _getButtonText(int index) {
+//     switch (clickCounts[index]) {
+//       case 1:
+//         return 'Present';
+//       case 2:
+//         return 'Absent';
+//       default:
+//         return 'Take';
+//     }
+//   }
+//
+//   final FirestoreService _firestoreService = FirestoreService();
+//   late TextEditingController _searchController;
+//   String? _selectedProgramTerm = "Sem - 6";
+//   String? _selectedDivision = "C";
+//   late String _searchTerm;
+//   ScrollController _dataController1 = ScrollController();
+//   ScrollController _dataController2 = ScrollController();
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _searchTerm = '';
+//     _searchController = TextEditingController();
+//     _UserIdDoc =
+//         FirebaseFirestore.instance.collection('metadata').doc('userId');
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Student List'),
+//       ),
+//       body: Column(
+//         children: [
+//           _buildFilters(),
+//           Expanded(
+//             child: _buildStudentList(),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+//
+//   Widget _buildFilters() {
+//     final String _selectedProgram = widget.program;
+//
+//     return Padding(
+//       padding: const EdgeInsets.all(8.0),
+//       child: Row(
+//         children: [
+//           Expanded(
+//             child: TextField(
+//               decoration: const InputDecoration(
+//                 labelText: 'Search',
+//                 hintText: 'Search by name',
+//                 border: OutlineInputBorder(),
+//               ),
+//               onChanged: (value) {
+//                 setState(() {
+//                   _searchTerm = value;
+//                 });
+//               },
+//             ),
+//           ),
+//           const SizedBox(width: 8),
+//           Text('Program : ${widget.program}'),
+//           const SizedBox(width: 8),
+//           DropdownButton<String>(
+//             value: _selectedProgramTerm,
+//             onChanged: (String? value) {
+//               setState(() {
+//                 _selectedProgramTerm = value!;
+//               });
+//             },
+//             items: _selectedProgram == ''
+//                 ? []
+//                 : _programTerm.map<DropdownMenuItem<String>>(
+//                     (String value) {
+//                       return DropdownMenuItem<String>(
+//                         value: value,
+//                         child: Text(value),
+//                       );
+//                     },
+//                   ).toList(),
+//             hint: const Text('Program Term'),
+//           ),
+//           const SizedBox(width: 8),
+//           DropdownButton<String>(
+//             value: _selectedDivision,
+//             onChanged: (String? value) {
+//               setState(() {
+//                 _selectedDivision = value!;
+//               });
+//             },
+//             items: _selectedProgramTerm == '--Please Select--'
+//                 ? []
+//                 : _selectedProgram == "BCA"
+//                     ? _Bcadivision.map((e) => DropdownMenuItem(
+//                           value: e,
+//                           child: Text(e),
+//                         )).toList()
+//                     : _selectedProgram == "B-Com"
+//                         ? _Bcomdivision.map((e) => DropdownMenuItem(
+//                               value: e,
+//                               child: Text(e),
+//                             )).toList()
+//                         : _Bbadivision.map((e) => DropdownMenuItem(
+//                               value: e,
+//                               child: Text(e),
+//                             )).toList(),
+//             hint: const Text('Class'),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+//
+//   Widget _buildStudentList() {
+//     final String _selectedProgram = widget.program;
+//     int rollnumber = 0;
+//     return StreamBuilder<List<Student>>(
+//       stream: _searchTerm.isEmpty
+//           ? _firestoreService.getStudents(
+//               _selectedProgram!, _selectedProgramTerm!, _selectedDivision!)
+//           : _firestoreService.searchStudents(_selectedProgram!,
+//               _selectedProgramTerm!, _selectedDivision!, _searchTerm),
+//       builder: (context, snapshot) {
+//         if (snapshot.hasError) {
+//           return Center(
+//             child: Text('Error: ${snapshot.error}'),
+//           );
+//         }
+//
+//         if (snapshot.connectionState == ConnectionState.waiting) {
+//           return const Center(
+//             child: CircularProgressIndicator(),
+//           );
+//         }
+//
+//         final students = snapshot.data;
+//
+//         if (students == null || students.isEmpty) {
+//           return const Center(
+//             child: Text('No students found'),
+//           );
+//         }
+//         if (clickCounts.isEmpty) {
+//           clickCounts = List.generate(students.length, (_) => 0);
+//         }
+//         return ListView.builder(
+//           itemCount: students.length,
+//           itemBuilder: (context, index) {
+//             final student = students[index];
+//             rollnumber++;
+//             return Card(
+//               child: ListTile(
+//                 leading: CircleAvatar(
+//                   child: Text(
+//                     '${rollnumber}',
+//                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+//                   ),
+//                 ),
+//                 title: Text(
+//                   student.firstname + " " + student.lastname,
+//                   style: TextStyle(fontWeight: FontWeight.bold),
+//                 ),
+//                 subtitle: Text(student.userId),
+//                 trailing: ElevatedButton(
+//                   style: ElevatedButton.styleFrom(
+//                       maximumSize: Size(100, 40),
+//                       backgroundColor: _getButtonColor(index),
+//                       minimumSize: Size(100, 40)),
+//                   onPressed: () {
+//                     _changeColorAndText(index);
+//                   },
+//                   child: Text(
+//                     _getButtonText(index),
+//                     style: TextStyle(
+//                         color: Colors.black, fontWeight: FontWeight.bold),
+//                   ),
+//                 ),
+//               ),
+//             );
+//           },
+//         );
+//       },
+//     );
+//   }
+// }
+
 class Attendance extends StatefulWidget {
   final program;
 
-  const Attendance({super.key, this.program});
-
-  void main() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-    //  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,overlays: SystemUiOverlay.values);
-  }
+  const Attendance({Key? key, this.program});
 
   @override
   _AttendanceState createState() => _AttendanceState();
 }
 
 class _AttendanceState extends State<Attendance> {
-  bool passwordObscured = true;
-  List<int> clickCounts = [];
-
-  void _changeColorAndText(int index) {
-    setState(() {
-      studentList[index]['clickCounts'] = (
-        studentList[index],
-        clickCounts[index] = (clickCounts[index] + 1) % 3
-      );
-    });
-  }
-
-  Color _getButtonColor(int index) {
-    switch (clickCounts[index]) {
-      case 1:
-        return Colors.green;
-      case 2:
-        return Colors.red;
-      default:
-        return Colors.white;
-    }
-  }
-
-  String _getButtonText(int index) {
-    switch (clickCounts[index]) {
-      case 1:
-        return 'Present';
-      case 2:
-        return 'Absent';
-      default:
-        return 'Take';
-    }
-  }
-
-  final FirestoreService _firestoreService = FirestoreService();
-  late TextEditingController _searchController;
-  String? _selectedProgramTerm = "Sem - 6";
-  String? _selectedDivision = "C";
+  late String _selectedProgramTerm;
+  late String _selectedDivision;
   late String _searchTerm;
-  ScrollController _dataController1 = ScrollController();
-  ScrollController _dataController2 = ScrollController();
+  late FirestoreService _firestoreService;
+  late TextEditingController _searchController;
+  late DocumentReference _UserIdDoc;
 
   @override
   void initState() {
     super.initState();
+    _selectedProgramTerm = 'Sem - 6';
+    _selectedDivision = 'C';
     _searchTerm = '';
+    _firestoreService = FirestoreService();
     _searchController = TextEditingController();
-    _UserIdDoc =
-        FirebaseFirestore.instance.collection('metadata').doc('userId');
+    _UserIdDoc = FirebaseFirestore.instance.collection('metadata').doc('userId');
   }
 
   @override
@@ -255,14 +444,13 @@ class _AttendanceState extends State<Attendance> {
   }
 
   Widget _buildFilters() {
-    final String _selectedProgram = widget.program;
-
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Row(
         children: [
           Expanded(
             child: TextField(
+              controller: _searchController,
               decoration: const InputDecoration(
                 labelText: 'Search',
                 hintText: 'Search by name',
@@ -285,16 +473,14 @@ class _AttendanceState extends State<Attendance> {
                 _selectedProgramTerm = value!;
               });
             },
-            items: _selectedProgram == ''
-                ? []
-                : _programTerm.map<DropdownMenuItem<String>>(
-                    (String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    },
-                  ).toList(),
+            items: _programTerm.map<DropdownMenuItem<String>>(
+                  (String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              },
+            ).toList(),
             hint: const Text('Program Term'),
           ),
           const SizedBox(width: 8),
@@ -305,22 +491,14 @@ class _AttendanceState extends State<Attendance> {
                 _selectedDivision = value!;
               });
             },
-            items: _selectedProgramTerm == '--Please Select--'
-                ? []
-                : _selectedProgram == "BCA"
-                    ? _Bcadivision.map((e) => DropdownMenuItem(
-                          value: e,
-                          child: Text(e),
-                        )).toList()
-                    : _selectedProgram == "B-Com"
-                        ? _Bcomdivision.map((e) => DropdownMenuItem(
-                              value: e,
-                              child: Text(e),
-                            )).toList()
-                        : _Bbadivision.map((e) => DropdownMenuItem(
-                              value: e,
-                              child: Text(e),
-                            )).toList(),
+            items: _Bcadivision.map<DropdownMenuItem<String>>(
+                  (String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              },
+            ).toList(),
             hint: const Text('Class'),
           ),
         ],
@@ -329,14 +507,12 @@ class _AttendanceState extends State<Attendance> {
   }
 
   Widget _buildStudentList() {
-    final String _selectedProgram = widget.program;
-    int rollnumber = 0;
     return StreamBuilder<List<Student>>(
       stream: _searchTerm.isEmpty
           ? _firestoreService.getStudents(
-              _selectedProgram!, _selectedProgramTerm!, _selectedDivision!)
-          : _firestoreService.searchStudents(_selectedProgram!,
-              _selectedProgramTerm!, _selectedDivision!, _searchTerm),
+          widget.program!, _selectedProgramTerm, _selectedDivision)
+          : _firestoreService.searchStudents(
+          widget.program!, _selectedProgramTerm, _selectedDivision, _searchTerm),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
           return Center(
@@ -361,35 +537,11 @@ class _AttendanceState extends State<Attendance> {
           itemCount: students.length,
           itemBuilder: (context, index) {
             final student = students[index];
-            rollnumber++;
-            return Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  child: Text(
-                    '${rollnumber}',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                ),
-                title: Text(
-                  student.firstname + " " + student.lastname,
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                subtitle: Text(student.userId),
-                trailing: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      maximumSize: Size(100, 40),
-                      backgroundColor: _getButtonColor(index),
-                      minimumSize: Size(100, 40)),
-                  onPressed: () {
-                    _changeColorAndText(index);
-                  },
-                  child: Text(
-                    _getButtonText(index),
-                    style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
+            return StudentListItem(
+              student: student,
+              onPressed: (isPresent) {
+                // Update the database or perform any other action based on the attendance status
+              },
             );
           },
         );
@@ -397,3 +549,52 @@ class _AttendanceState extends State<Attendance> {
     );
   }
 }
+
+class StudentListItem extends StatefulWidget {
+  final Student student;
+  final Function(bool) onPressed;
+
+  const StudentListItem({
+    Key? key,
+    required this.student,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  _StudentListItemState createState() => _StudentListItemState();
+}
+
+class _StudentListItemState extends State<StudentListItem> {
+  bool isPresent = true; // Assuming present by default
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          widget.student.firstname + " " + widget.student.lastname,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text(widget.student.userId),
+        trailing: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            maximumSize: Size(100, 40),
+            backgroundColor: isPresent ? Colors.green : Colors.red,
+            minimumSize: Size(100, 40),
+          ),
+          onPressed: () {
+            setState(() {
+              isPresent = !isPresent;
+              widget.onPressed(isPresent);
+            });
+          },
+          child: Text(
+            isPresent ? 'Present' : 'Absent',
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
